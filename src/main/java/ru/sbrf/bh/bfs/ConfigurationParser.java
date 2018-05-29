87e32885fbe3c3a513b90bfa8d69d9e8021a8c70
Барсуков Сергей Васильевич <SBT-Barsukov-SV@mail.ca.sbrf.ru>
2018-05-29 13:12:37 +0300

    -import static

--- src/main/java/ru/sbrf/bh/bfs/ConfigurationParser.java
+++ src/main/java/ru/sbrf/bh/bfs/ConfigurationParser.java
@@ -1,6 +1,6 @@
 package ru.sbrf.bh.bfs;
 
-import static ru.sbrf.bh.bfs.generator.StringConsts.*;
+import ru.sbrf.bh.bfs.generator.StringConsts;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -37,46 +37,46 @@ public class ConfigurationParser {
         List<Api> apis = new ArrayList<Api>();
 
         for (BfsParser.GroupContext gc : ((BfsParser.RContext) tree).group()) {
-            if (PROPERTIES.equals(gc.ID().getText())) {
+            if (StringConsts.PROPERTIES.equals(gc.ID().getText())) {
                 for (BfsParser.PropertyContext pc : gc.property()) {
                     p.put(pc.ID().getText(), trm(pc.STRING().getText()));
                 }
             }
-            if (BUILD.equalsIgnoreCase(gc.ID().getText())) {
+            if (StringConsts.BUILD.equalsIgnoreCase(gc.ID().getText())) {
                 for (BfsParser.PropertyContext pc : gc.property()) {
                     b.put(pc.ID().getText(), trm(pc.STRING().getText()));
                 }
             }
-            if (ADAPTERS.equals(gc.ID().getText())) {
+            if (StringConsts.ADAPTERS.equals(gc.ID().getText())) {
                 for (BfsParser.GroupContext igc : gc.group()) {
                     String groupId = null, artifactId = null, version = null;
                     for (BfsParser.PropertyContext pc : igc.property()) {
                         String text = trm(pc.STRING().getText());
                         String name = pc.ID().getText();
 
-                        if (GROUP_ID.equals(name)) groupId = text;
-                        if (ARTIFACT_ID.equals(name)) artifactId = text;
-                        if (VERSION.equals(name)) version = text;
+                        if (StringConsts.GROUP_ID.equals(name)) groupId = text;
+                        if (StringConsts.ARTIFACT_ID.equals(name)) artifactId = text;
+                        if (StringConsts.VERSION.equals(name)) version = text;
                     }
                     if (groupId != null && artifactId != null && version != null) {
                         ad.add(new Adapter(groupId, artifactId, version));
                     }
                 }
             }
-            if (API.equals(gc.ID().getText())) {
+            if (StringConsts.API.equals(gc.ID().getText())) {
                 for (BfsParser.GroupContext igc : gc.group()) {
                     String fgClass = null, daClass = null, rq = null, rs = null, service = null, methodName = null, apiName = igc.ID().getText();
                     for (BfsParser.PropertyContext pc : igc.property()) {
                         String text = trm(pc.STRING().getText());
                         String name = pc.ID().getText();
 
-                        if (FG_CLASS.equals(name)) fgClass = text;
-                        if (DA_CLASS.equals(name)) daClass = text;
-                        if (RQ.equals(name)) rq = text;
-                        if (RS.equals(name)) rs = text;
-                        if (SERVICE.equals(name)) service = text;
-                        if (METHOD_NAME.equals(name)) methodName = text;
-                        if (NAME.equals(name)) name = text;
+                        if (StringConsts.FG_CLASS.equals(name)) fgClass = text;
+                        if (StringConsts.DA_CLASS.equals(name)) daClass = text;
+                        if (StringConsts.RQ.equals(name)) rq = text;
+                        if (StringConsts.RS.equals(name)) rs = text;
+                        if (StringConsts.SERVICE.equals(name)) service = text;
+                        if (StringConsts.METHOD_NAME.equals(name)) methodName = text;
+                        if (StringConsts.NAME.equals(name)) name = text;
                     }
                     if (fgClass != null && daClass != null && rq != null
                             && rs != null
